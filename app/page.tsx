f3181a85e80b496d31cd5897d1525e93bf43772e
Dimmaweb20 <chidimaumeayo@gmail.com>
2025-07-09 16:39:29 +0100

    fixing document

--- app/page.tsx
+++ app/page.tsx
@@ -3,22 +3,20 @@
 import About from '@/components/About'
 import Aside from '@/components/Aside'
 import Loader from '@/components/Loader'
-import Resume from '@/components/Resume'
-import Image from 'next/image'
 import pageanim from '@/animations/pageanim.json';
-import { useEffect, useState, useContext } from 'react'
+import dynamic from 'next/dynamic';
+
+const DynamicLoader = dynamic(() => import('@/components/Loader'), { ssr: false })
 
 export default function Home() {
-
-  const [value, setValue] = useState()
-
   return (
     <>
       <main className='w-full flex mt-32 gap-5'>
         <Aside />
-        <Loader animationData={pageanim}>
+        <DynamicLoader animationData={pageanim}>
           <About />
-        </Loader>
+        </DynamicLoader>
+
       </main>
     </>
   )
